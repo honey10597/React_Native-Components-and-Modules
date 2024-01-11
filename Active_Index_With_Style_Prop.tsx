fdd9c4b722bfc75c525a4d946cce726e042edd9f
Honey Kumar <42267565+honey10597@users.noreply.github.com>
2024-01-11 15:22:01 +0530

    Create Active_Index_With_Style_Prop.tsx

---
+++ Active_Index_With_Style_Prop.tsx
@@ -0,0 +1,28 @@
+ <ScrollView
+        horizontal
+        style={{
+          marginTop: moderateScale(6),
+          alignSelf: "center"
+        }}
+        contentContainerStyle={{ alignItems: "center" }}
+      >
+        {state?.data.map((item, index) => {
+          return (
+            <View style={styles.activeIndexView(index, state?.activeIndex)} />
+          )
+        })}
+      </ScrollView>
+
+const styles = StyleSheet.create<any>({
+  activeIndexView: (currentIndex: number, activeIndex: number) => ({
+    marginTop: moderateScale(20),
+    height: moderateScale(10),
+    width: currentIndex === activeIndex ? moderateScale(50) : moderateScale(10),
+    borderRadius: moderateScale(5),
+    marginStart: moderateScale(5),
+    backgroundColor:
+      currentIndex === activeIndex ?
+        colors.themeBlue :
+        colors.grey_217_1
+  })
+})
