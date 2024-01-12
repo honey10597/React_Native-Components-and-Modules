69490801b8606a1cf8549e6d8b0f4f17c8a76839
Honey Kumar <42267565+honey10597@users.noreply.github.com>
2024-01-12 17:29:22 +0530

    Update Corousal_Horizontal_Onboarding.tsx

--- Corousal_Horizontal_Onboarding.tsx
+++ Corousal_Horizontal_Onboarding.tsx
@@ -6,20 +6,34 @@
   }
 
 
- <ScrollView
-        horizontal
-        style={{
-          marginTop: moderateScale(6),
-          alignSelf: "center"
-        }}
-        contentContainerStyle={{ alignItems: "center" }}
-      >
-        {state?.data.map((item, index) => {
-          return (
-            <View style={styles.activeIndexView(index, state?.activeIndex)} />
-          )
-        })}
-      </ScrollView>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+<View style={{ height: "85%" }}>
+        <FlatList
+          bounces={false}
+          ref={flatListRef}
+          data={state?.data}
+          pagingEnabled={true}
+          horizontal
+          keyExtractor={(item: any, index: number) => String(item + index)}
+          onScroll={_onScroll}
+          showsHorizontalScrollIndicator={false}
+          renderItem={_renderOnboarding}
+        />
+        <ScrollView
+          horizontal
+          style={styles.scrollStyle}
+          contentContainerStyle={{ alignItems: "center" }}
+        >
+          {state?.data.map((item, index) => {
+            return (
+              <View style={styles.activeIndexView(index, state?.activeIndex)} />
+            )
+          })}
+        </ScrollView>
+      </View>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const styles = StyleSheet.create<any>({
   activeIndexView: (currentIndex: number, activeIndex: number) => ({
